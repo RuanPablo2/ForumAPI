367683abd3faaf9e15dff8a3d8e4105f08ca7494
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-03-26 01:07:13 -0300

    Model User and REST working

---
+++ src/main/java/com/RuanPablo2/ForumAPI/model/User.java
@@ -0,0 +1,36 @@
+package com.RuanPablo2.ForumAPI.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
