3bb1be7984beabb01bcab0e746531f617757741d
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-03-29 00:48:04 -0300

    SecurityConfig, Userdetails and JWT

--- src/main/java/com/RuanPablo2/ForumAPI/repositories/UserRepository.java
+++ src/main/java/com/RuanPablo2/ForumAPI/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.RuanPablo2.ForumAPI.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/RuanPablo2/ForumAPI/security/CustomUserDetails.java
@@ -0,0 +1,79 @@
+package com.RuanPablo2.ForumAPI.security;
+
+import com.RuanPablo2.ForumAPI.model.User;
+import com.RuanPablo2.ForumAPI.model.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+        this.authorities = Arrays.asList(new SimpleGrantedAuthority(user.getRole().toString()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public Role getRole() {
+        return role;
+    }
+}
